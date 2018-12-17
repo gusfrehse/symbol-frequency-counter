c9062e14009034c2132e7c5e8d246cb3bef47a24
Gustavo Silveira Frehse <gusfrehse@gmail.com>
2018-12-17 19:09:49 -0200

    made first code that only prints the frequencies, in a non formatted way

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+if (len(sys.argv) < 2):
+    print("Usage: main.py <pathToInputFile>")
+    quit(1)
+
+with open(sys.argv[1]) as f:
+    txt = f.read()
+
+characters = {}
+numOfChars = len(txt)
+
+for c in txt:
+    if c not in characters:
+        characters[c] = 1
+    else:
+        characters[c] += 1
+
+for k,v in characters.items():
+    characters[k]  = v / numOfChars
+
+print(characters) 
